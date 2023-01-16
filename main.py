8e69ed79a697b6345ac711082563edc3ec71e45b
joris-wes <99329100+joris-wes@users.noreply.github.com>
2023-01-16 22:44:43 +0100

    Added a check if the file is changed or not

--- main.py
+++ main.py
@@ -10,32 +10,41 @@ docs = db.view('_all_docs', include_docs=True)
 global count
 count = 0
 
+global replaced 
+replaced = False
+
 def check_and_replace_temp(doc):
     global count
+    global replaced
     if isinstance(doc, dict):
-        if "TempC_SHT" in doc:
-            doc["temperature"] = doc.pop("TempC_SHT")
+        if "temperature" in doc:
+            doc["Temperature"] = doc.pop("temperature")
             count = count + 1
+            replaced = True
         else:
             for key in doc:
                 check_and_replace_temp(doc[key])
 
 def check_and_replace_hum(doc):
     global count
+    global replaced
     if isinstance(doc, dict):
         if "Hum_SHT" in doc:
             doc["humidity"] = doc.pop("Hum_SHT")
             count = count + 1
+            replaced = True
         else:
             for key in doc:
                 check_and_replace_hum(doc[key])
 
 def check_and_replace_ill(doc):
     global count
+    global replaced
     if isinstance(doc, dict):
         if "ILL_lx" in doc:
             doc["illumination"] = doc.pop("ILL_lx")
             count = count + 1
+            replaced = True
         else:
             for key in doc:
                 check_and_replace_ill(doc[key])
@@ -43,8 +52,11 @@ def check_and_replace_ill(doc):
 
 for doc in docs:
     doc_data = doc.doc
+    replaced = False
     check_and_replace_temp(doc_data)
     check_and_replace_hum(doc_data)
     check_and_replace_ill(doc_data)
-    db.save(doc_data)
+    if replaced:
+        db.save(doc_data)
+        print("Replaced")
     print(count)
